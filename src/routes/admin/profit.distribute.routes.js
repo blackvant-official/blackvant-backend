5fca130dbd1e70fc1855e742f3c0a2f733d87057
blackvant-official <theblackvant@gmail.com>
2026-01-05 21:35:18 +0500

    Update profit.distribute.routes.js

--- src/routes/admin/profit.distribute.routes.js
+++ src/routes/admin/profit.distribute.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import prisma from "../../../prisma.js";
+import prisma from "../../../utils/prisma.js";
 import { requireAuth } from "../../middleware/auth.js";
 
 const router = express.Router();
