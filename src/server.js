98ca7e52ba9da6dd3d1c7bb835106f24f08cee9c
blackvant-official <theblackvant@gmail.com>
2026-01-05 18:34:01 +0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -75,7 +75,7 @@ import auditAdminRoutes from "./routes/admin/audit.admin.routes.js";
 import ledgerAdminRoutes from "./routes/admin/ledger.admin.routes.js";
 import adminEmailTestRoutes from "./routes/admin/email.test.routes.js";
 import adminProfitOtpRoutes from "./routes/admin/profit.otp.routes.js";
-import adminProfitOtpRoutes from "./routes/admin/profit.otp.routes.js";
+
 
 
 app.use("/api/v1", userRoutes);
@@ -94,7 +94,7 @@ app.use("/api/v1/admin", auditAdminRoutes);
 app.use("/api/v1/admin/ledger", ledgerAdminRoutes);
 app.use("/api/v1/admin", adminEmailTestRoutes);
 app.use("/api/v1/admin", adminProfitOtpRoutes);
-app.use("/api/v1/admin", adminProfitOtpRoutes);
+
 
 app.use((err, req, res, next) => {
   console.error("🔥 GLOBAL ERROR:", err);
