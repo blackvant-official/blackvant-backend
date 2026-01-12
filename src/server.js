11403338efbe6a8bbb9700dcce3aa2ce30d615ad
blackvant-official <theblackvant@gmail.com>
2026-01-12 11:39:17 +0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -27,7 +27,7 @@ app.use(
       return callback(new Error("CORS not allowed"), false);
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
